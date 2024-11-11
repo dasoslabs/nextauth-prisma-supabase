f753f49311be2b3d0bb17c990847fd8408fa0e4c
dasoslabs <dasoslab@gmail.com>
2024-11-11 17:35:05 +0900

    🏷️ prisma 타입 지정

--- src/prisma/index.js
+++ src/prisma/index.js
@@ -1,9 +1,13 @@
+// @ts-check
 import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = global;
-
+/** @type {PrismaClient} */
 export const prisma = globalForPrisma.prisma || new PrismaClient();
 
-if (process.env.NODE_ENV !== "production") {
+/** @type {NodeJS.ProcessEnv} */
+const env = process.env;
+
+if (env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
 }
