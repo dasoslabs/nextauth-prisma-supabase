a6735161b4817e42ae7f95708b34cc2b6b3ea478
dasoslabs <dasoslab@gmail.com>
2024-11-11 13:21:48 +0900

    🔧 Prisma 세팅

--- src/server/index.js
+++ src/server/index.js
@@ -6,7 +6,6 @@ import { oauthProvider } from "@/constants";
 export const signInWithProvider = async (formData) => {
   const provider = formData.get("provider") ?? oauthProvider.google;
 
-  // redirect 콜백을 실행시키려면 redirectTo 옵션 제거
   await signIn(provider, { redirectTo: "/protected" });
 };
 
